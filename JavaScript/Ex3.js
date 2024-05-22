c2d93d35f8f65704981beecd435604babb99854f
Muzammil71 <zamiikhan713@gmail.com>
2024-05-23 01:34:17 +0500

    Average Number

---
+++ JavaScript/Ex3.js
@@ -0,0 +1,5 @@
+var firstNumber = 704;
+var secondNumber = 404;
+var average = firstNumber/secondNumber
+
+document.getElementById('average_numbers').innerHTML = "The average of two number is " + (average.toFixed(2));
