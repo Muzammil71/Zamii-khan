ff03c75d7869076b43c92530a79f54f63ef96c9b
Muzammil71 <zamiikhan713@gmail.com>
2024-05-23 17:46:54 +0500

    Array in ordered from lowest to highest

---
+++ JavaScript/Exercise 2/Exr2.js
@@ -0,0 +1,3 @@
+var classification = ["John Hill" , "Mary Jane", "Gary Vee", "Paricia Mills", "Helen Hall", "Paul Green"];
+var best_students = classification.slice(-3);
+console.log("Three best students are:", best_students);
