7fcd4a2953b0eb8fc9cb1cfa0ebb3ad37eea6757
Muzammil71 <zamiikhan713@gmail.com>
2024-05-23 11:40:49 +0500

    URLS

---
+++ JavaScript/Ex.8.js
@@ -0,0 +1,5 @@
+var url="www.google.com";
+document.getElementById("Url").innerHTML="Replace URL www.google.com by  " + (url.replace("www.google.com" , "https://www.google.com"));
+
+var Url1="http://www.google.com";
+document.getElementById("Url1").innerHTML="Replace URL http://www.google.com by  " + (url.replace("http://www.google.com" , "www.google.com"));
